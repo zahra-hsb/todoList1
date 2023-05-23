4eeda0a83c13f2d23b43025a9425938b20e37a84
zahra-hsb <zahra16toa@gmail.com>
2023-05-24 00:05:56 +0430

    add button select and eventlistener codes added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const items = document.querySelectorAll("li");
+const addBtn = document.querySelector("button");
 
 items.forEach(item => {
     item.addEventListener("click", e => {
         // e.target.classList.add("line-through");
         e.target.remove();
     })
+});
+
+addBtn.addEventListener("click", e=>{
+
 });
