38bf7e5838e81ecf5030a910c87c6c7eb70bc509
zahra-hsb <zahra16toa@gmail.com>
2023-05-24 00:22:21 +0430

    change place of add li tag code

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const items = document.querySelectorAll("li");
 const addBtn = document.querySelector("button");
 const ul = document.querySelector("ul");
 
+addBtn.addEventListener("click", ()=>{
+    const li = document.createElement("li");
+    li.textContent = 'zahra';
+    ul.prepend(li)
+});
+
 items.forEach(item => {
     item.addEventListener("click", e => {
         // e.target.classList.add("line-through");
         e.target.remove();
     })
-});
-
-addBtn.addEventListener("click", ()=>{
-    const li = document.createElement("li");
-    li.textContent = 'zahra';
-    ul.prepend(li)
 });
