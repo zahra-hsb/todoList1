7b4f28e893d36f590e9975b3a8c10a679c34b7f6
zahra-hsb <zahra16toa@gmail.com>
2023-05-23 23:30:56 +0430

    remove element code added for remove them when these element clicked

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const items = document.querySelectorAll("li");
 
 items.forEach(item => {
     item.addEventListener("click", e => {
-        e.target.classList.add("line-through");
+        // e.target.classList.add("line-through");
+        e.target.remove();
     })
 });
